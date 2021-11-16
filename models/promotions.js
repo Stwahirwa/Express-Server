9081ed8720c7c913aab891cbda631be2a0377752
Stwahirwa <stwahirwa91@gmail.com>
2021-11-15 19:49:12 -0600

    Basic Authentication

---
+++ models/promotions.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+require('mongoose-currency').loadType(mongoose);
+
+const Schema = mongoose.Schema;
+const Currency = mongoose.Types.Currency;
+
+
+const promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        default: true
+    },
+    cost:{
+        type: Currency,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const Promotion = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotion;
