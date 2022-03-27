c4aefe1488b3c10954fb98c1af76e26fdb80cd81
Sam Twahirwa <87049175+Stwahirwa@users.noreply.github.com>
2022-03-27 18:56:50 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '416479233275698',
-        clientSecret: '438fd73120f854baf4b539f810075341'
-    }
-}
